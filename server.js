67940d268e01de8cddafe9071323fb55d3b78f2f
Elliot Spencer <135474191+ElliotSpencer1@users.noreply.github.com>
2025-02-04 09:35:11 +0000

    server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  socket.on('playerMove', (data) => {
+    socket.broadcast.emit('playerMove', data);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
